d52f62db63eacb388299dff4e1e4c9fda2534c00
Brandon Boswell <brandonkboswell@gmail.com>
2023-10-01 00:34:18 -0400

    Fixed an issue where Escape + Escape + J and K would move lines around

---
+++ nvim/lua/plugins/lazyvim.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      defaults = {
+        -- autocmds = false,  -- works
+        -- options = false,  -- does not work
+        keymaps = false,  -- works
+      },
+    },
+  },
+}
