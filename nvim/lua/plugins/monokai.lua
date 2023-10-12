ec2264425d02e80f27efd1218ef84c05ed2f1003
Brandon Boswell <brandonkboswell@gmail.com>
2023-10-11 20:09:54 -0400

    Remove Monokai Underline

--- nvim/lua/plugins/monokai.lua
+++ nvim/lua/plugins/monokai.lua
@@ -34,12 +34,12 @@ return {
         "notify",
         -- "nvim-tree",
         -- "neo-tree",
-        -- "bufferline", -- better used if background of `neo-tree` or `nvim-tree` is cleared
+        "bufferline", -- better used if background of `neo-tree` or `nvim-tree` is cleared
       },-- "float_win", "toggleterm", "telescope", "which-key", "renamer", "neo-tree", "nvim-tree", "bufferline"
       plugins = {
         bufferline = {
-          underline_selected = true,
-          underline_visible = true,
+          underline_selected = false,
+          underline_visible = false,
         },
         indent_blankline = {
           context_highlight = "default", -- default | pro
