661d38a357bda9599690be509c487c3bb085532c
Brandon Boswell <brandonkboswell@gmail.com>
2023-10-15 14:13:59 -0400

    Thank you to Josh for helping me get over my Telescope Woes

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -4,24 +4,29 @@ return {
     "AckslD/nvim-neoclip.lua",
     "ThePrimeagen/harpoon",
     "nvim-telescope/telescope-live-grep-args.nvim" ,
+    "princejoogie/dir-telescope.nvim",
   },
   -- apply the config and additionally load fzf-native
   config = function(_)
     local telescope = require("telescope")
-    local lga_actions = require("telescope-live-grep-args.actions")
 
     telescope.setup({
       defaults = {
         mappings = {
+          n = {
+            ["<C-w>"] = require('telescope.actions').send_selected_to_qflist + require('telescope.actions').open_qflist,
+          },
           i = {
             ['<C-p>'] = require('telescope.actions.layout').toggle_preview,
             ['<C-j>'] = require('telescope.actions').cycle_history_next,
             ['<C-k>'] = require('telescope.actions').cycle_history_prev,
+            ["<C-w>"] = require('telescope.actions').send_selected_to_qflist + require('telescope.actions').open_qflist,
           },
         },
         preview = {
-          hide_on_startup = true -- hide previewer when picker starts
+          hide_on_startup = false
         },
+        sorting_strategy = "ascending",
         file_ignore_patterns = { ".git/", "node_modules", "vendor" },
         layout_config = {
           height = 0.90,
@@ -36,8 +41,8 @@ return {
         live_grep_args = {
           mappings = {
             i = {
-              ['<C-c>'] = lga_actions.quote_prompt({ postfix = " --iglob !.spec"}),
-              ['<C-i>'] = lga_actions.quote_prompt({ postfix = " --iglob !.spec"}),
+              ['<C-c>'] = require("telescope-live-grep-args.actions").quote_prompt({ postfix = " --iglob !.spec"}),
+              ['<C-i>'] = require("telescope-live-grep-args.actions").quote_prompt({ postfix = " --iglob !.spec"}),
             },
           },
         },
@@ -47,14 +52,14 @@ return {
     telescope.load_extension("harpoon")
     telescope.load_extension("neoclip")
     telescope.load_extension("live_grep_args")
+    telescope.load_extension("dir")
   end,
 
   keys = {
     { "<leader>fg", "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", desc = "Live Grep (args)" },
     { "<leader>sg", "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", desc = "Live Grep (args)" },
     { "<leader>fc", "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args({ default_text = '\"model\" --ignore-file \"*.spec|*.test.js\"' })<CR>", desc = "Live Grep (code)" },
-    -- { "<leader>*", "<cmd>lua require('telescope').extensions.live_grep_args.shortcuts.grep_word_under_cursor<CR>", desc = "Grep Word Under Cursor" },
-    -- { "<leader>v*", "<cmd>lua require('telescope').extensions.live_grep_args.shortcuts.grep_visual_selection<CR>", desc = "Grep Word Visual Selection" },
-    -- { "<leader>v*", "<cmd>Telescope grep_string<cr>", desc = "Grep Word Under Cursor" },
+    { "<leader>fh", "<cmd>Telescope harpoon marks<CR>", desc = "Find Harpoon Marks" },
+    { "<leader>fd", "<cmd>Telescope dir live_grep<CR>", desc = "Grep Directory"}
   },
 }
